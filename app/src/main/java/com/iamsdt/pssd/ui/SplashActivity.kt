e0267db213f22de43c5f5d4f3997b1a4023b9b18
Iamsdt <shudiptotrafder@gmail.com>
2018-08-17 12:49:01 +0600

    change package
    Took 2 minutes

---
+++ app/src/main/java/com/iamsdt/pssd/ui/SplashActivity.kt
@@ -0,0 +1,19 @@
+/*
+ * Developed By Shudipto Trafder
+ * on 8/17/18 12:41 PM
+ * Copyright (c) 2018 Shudipto Trafder.
+ */
+
+package com.iamsdt.pssd.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iamsdt.pssd.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
