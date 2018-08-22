32a9c48a8defdce30b0ba5ce8f66bc77e3210958
Shudipto <Shudiptotrafder@gmail.com>
2018-08-22 16:43:10 +0600

    Add search to main activity
    Took 11 minutes

--- app/src/main/java/com/iamsdt/pssd/ui/main/MainActivity.kt
+++ app/src/main/java/com/iamsdt/pssd/ui/main/MainActivity.kt
@@ -6,30 +6,38 @@
 
 package com.iamsdt.pssd.ui.main
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.SearchRecentSuggestions
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.iamsdt.pssd.R
 import com.iamsdt.pssd.ext.ToastType
 import com.iamsdt.pssd.ext.ViewModelFactory
 import com.iamsdt.pssd.ext.showToast
 import com.iamsdt.pssd.ext.toNextActivity
+import com.iamsdt.pssd.ui.details.DetailsActivity
 import com.iamsdt.pssd.ui.favourite.FavouriteActivity
 import com.iamsdt.pssd.ui.flash.FlashCardActivity
-import com.iamsdt.pssd.ui.search.SearchActivity
+import com.iamsdt.pssd.ui.search.MySuggestionProvider
 import com.iamsdt.pssd.ui.settings.SettingsActivity
+import com.iamsdt.pssd.utils.Constants
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +48,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private val viewModel: MainVM by lazy {
         ViewModelProviders.of(this, factory).get(MainVM::class.java)
     }
+
+    private var suggestions: SearchRecentSuggestions? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,16 +65,36 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val deco= DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         deco.setDrawable(resources.getDrawable(R.drawable.dercoration))
 
-        // TODO: 8/22/18 add item decoration
+        // Complete: 8/22/18 add item decoration
+        mainRcv.addItemDecoration(deco)
 
-        viewModel.getData().observe(this, Observer {
+        viewModel.liveData.observe(this, Observer {
             adapter.submitList(it)
         })
 
+        viewModel.event.observe(this, Observer {
+            it?.let {
+                if (it.title == Constants.SEARCH){
+                    if (it.status){
+                        Timber.i("Status is true")
+                        val intent = Intent(this, DetailsActivity::class.java)
+                        intent.putExtra(Intent.EXTRA_TEXT, it.message.toInt())
+                        startActivity(intent)
+                    } else{
+                        Timber.i(it.message)
+                        showToast(ToastType.ERROR,it.message)
+                    }
+                }
+            }
+        })
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            // TODO: 8/22/18 add random layout
         }
+
+        suggestions = SearchRecentSuggestions(this,
+                MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE)
+        handleSearch(intent)
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -71,6 +102,33 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun setRecentQuery(query: String) {
+        suggestions?.saveRecentQuery(query, null)
+
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        handleSearch(intent)
+        Timber.i("Call")
+    }
+
+    private fun handleSearch(intent: Intent){
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            // complete: 6/14/2018 search
+            viewModel.submit(query)
+            setRecentQuery(query)
+
+//            //Search
+//            val bundle = Bundle()
+//            bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "Search Data")
+//            bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "User search query")
+//            bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "Search $query")
+//            FirebaseAnalytics.getInstance(this)
+//                    .logEvent(FirebaseAnalytics.Event.SEARCH,bundle)
+        }
     }
 
     override fun onBackPressed() {
@@ -84,6 +142,49 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
+
+        val searchView = menu.findItem(R.id.search)?.actionView as SearchView
+        //search
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false)
+        searchView.isQueryRefinementEnabled = true
+        searchView.requestFocus()
+
+        searchView.setOnSuggestionListener(object : SearchView.OnSuggestionListener {
+            override fun onSuggestionSelect(position: Int): Boolean {
+                Timber.i("call")
+                return true
+            }
+
+            override fun onSuggestionClick(position: Int): Boolean {
+                Timber.i("call")
+                val selectedView = searchView.suggestionsAdapter
+                val cursor = selectedView.getItem(position) as Cursor
+                val index = cursor.getColumnIndexOrThrow(SearchManager.SUGGEST_COLUMN_TEXT_1)
+                searchView.setQuery(cursor.getString(index), true)
+                return true
+            }
+        })
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Timber.i("call")
+                viewModel.submit(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Timber.i("call")
+                newText?.let {
+                    Timber.i("new text is $newText")
+                    viewModel.requestSearch(it)
+                }
+                return true
+            }
+
+        })
+
         return true
     }
 
@@ -94,10 +195,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return when (item.itemId) {
             R.id.action_settings -> {
                 toNextActivity(SettingsActivity::class)
-                true
-            }
-            R.id.action_search -> {
-                toNextActivity(SearchActivity::class)
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/com/iamsdt/pssd/ui/main/MainVM.kt
+++ app/src/main/java/com/iamsdt/pssd/ui/main/MainVM.kt
@@ -6,17 +6,33 @@
 
 package com.iamsdt.pssd.ui.main
 
-import androidx.lifecycle.LiveData
+import android.os.AsyncTask
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.iamsdt.pssd.database.WordTable
 import com.iamsdt.pssd.database.WordTableDao
+import com.iamsdt.pssd.ext.SingleLiveEvent
+import com.iamsdt.pssd.utils.Constants
+import com.iamsdt.pssd.utils.model.StatusModel
+import timber.log.Timber
 import javax.inject.Inject
 
-class MainVM @Inject constructor(val wordTableDao: WordTableDao):ViewModel(){
+class MainVM @Inject constructor(val wordTableDao: WordTableDao) : ViewModel() {
 
-    fun getData(): LiveData<PagedList<WordTable>> {
+    val event = SingleLiveEvent<StatusModel>()
+
+    lateinit var liveData: MediatorLiveData<PagedList<WordTable>>
+
+    init {
+        if (!::liveData.isInitialized) {
+            liveData = MediatorLiveData()
+            getData()
+        }
+    }
+
+    private fun getData() {
 
         val source = wordTableDao.getAllData()
 
@@ -28,7 +44,48 @@ class MainVM @Inject constructor(val wordTableDao: WordTableDao):ViewModel(){
                 .build()
 
 
-        return LivePagedListBuilder(source, config).build()
+        val date = LivePagedListBuilder(source, config)
+                .build()
+
+        liveData.addSource(date) {
+            liveData.value = it
+        }
+    }
+
+    fun requestSearch(query: String) {
+
+        if (query.isEmpty()) return
+
+        val source = wordTableDao.getSearchData(query)
+
+        val config = PagedList.Config.Builder()
+                .setPageSize(10)
+                .setInitialLoadSizeHint(20)//by default page size * 3
+                .setPrefetchDistance(10) // default page size
+                .setEnablePlaceholders(true) //default true
+                .build()
+
+
+        val data = LivePagedListBuilder(source, config).build()
+
+        liveData.addSource(data) {
+            liveData.value = it
+        }
+    }
+
+
+    fun submit(query: String?) {
+        query?.let {
+            AsyncTask.execute {
+                val word: WordTable? = wordTableDao.getSearchResult(it)
+                Timber.i("Word:$word")
+                if (word != null) {
+                    event.postValue(StatusModel(true, Constants.SEARCH, "${word.id}"))
+                } else {
+                    event.postValue(StatusModel(false, Constants.SEARCH, "Word not found!"))
+                }
+            }
+        }
     }
 
 }
