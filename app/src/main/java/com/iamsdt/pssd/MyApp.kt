a2d88ac88c8b97dd667eb7fa275dc11b9161dee3
Shudipto <Shudiptotrafder@gmail.com>
2018-11-13 13:16:57 +0600

    crashlytics
    Took 5 minutes

--- app/src/main/java/com/iamsdt/pssd/MyApp.kt
+++ app/src/main/java/com/iamsdt/pssd/MyApp.kt
@@ -10,12 +10,14 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import com.crashlytics.android.Crashlytics
 import com.iamsdt.pssd.di.appModule
 import com.iamsdt.pssd.di.dbModule
 import com.iamsdt.pssd.di.vmModule
 import com.iamsdt.pssd.ext.DebugLogTree
 import com.iamsdt.pssd.ext.ReleaseLogTree
 import com.rohitss.uceh.UCEHandler
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
@@ -24,16 +26,19 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        UCEHandler.Builder(applicationContext).build()
+        Fabric.with(applicationContext, Crashlytics())
 
-        if (BuildConfig.DEBUG) Timber.plant(DebugLogTree())
-        else Timber.plant(ReleaseLogTree())
+        if (BuildConfig.DEBUG) {
+            UCEHandler.Builder(applicationContext).build()
+            Timber.plant(DebugLogTree())
+        }
 
         startKoin(this, listOf(dbModule, appModule, vmModule))
 
         createNotificationChannel()
     }
 
+    //create notification channel
     private fun createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
