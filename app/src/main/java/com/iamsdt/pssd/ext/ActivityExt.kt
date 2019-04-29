d9f1ec8dfaa96a0312656e3953fd41b722564bc8
Shudipto <Shudiptotrafder@gmail.com>
2019-04-29 10:56:56 +0600

    update extension function
    Took 1 minute

--- app/src/main/java/com/iamsdt/pssd/ext/ActivityExt.kt
+++ app/src/main/java/com/iamsdt/pssd/ext/ActivityExt.kt
@@ -8,51 +8,71 @@ package com.iamsdt.pssd.ext
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import com.iamsdt.pssd.R
-import kotlin.reflect.KClass
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
 
+inline fun <reified T : AppCompatActivity> AppCompatActivity.nextActivity(
+        extraKey: String = "", extra: Any = "", finish: Boolean = false
+) {
 
-fun AppCompatActivity.runThread(timer: Long, clazz: KClass<out AppCompatActivity>) =
-        Thread {
-            try {
-                Thread.sleep(timer)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            } finally {
-                toNextActivity(clazz,finish = true)
-            }
-        }.start()
-
-fun AppCompatActivity.toNextActivity(
-        clazz: KClass<out AppCompatActivity>,
-        extraKey: String = "",
-        extra: String = "",
-        finish: Boolean = false) {
-    val intent = Intent(this, clazz.java)
+    val intent = Intent(this, T::class.java)
 
     if (extraKey.isNotEmpty()) {
-        intent.putExtra(extraKey, extra)
+        when (extra) {
+            is String -> intent.putExtra(extraKey, extra)
+            is Boolean -> intent.putExtra(extraKey, extra)
+            is Float -> intent.putExtra(extraKey, extra)
+            is Long -> intent.putExtra(extraKey, extra)
+            is Int -> intent.putExtra(extraKey, extra)
+            is Bundle -> intent.putExtra(extraKey, extra)
+            else -> intent.putExtra(extraKey, "$extra")
+        }
     }
 
     startActivity(intent)
 
-    if (finish) {
+    if (finish) finish()
+}
+
+inline fun <reified T : AppCompatActivity> AppCompatActivity.runThread(timer: Long = 1000) {
+    thread(priority = 10) {
+        sleep(timer)
+        nextActivity<T>()
         finish()
     }
 }
 
-fun Fragment.toNextActivity(
-        clazz: KClass<out AppCompatActivity>,
-        extraKey: String = "",
-        extra: String = "") {
+inline fun <reified T : AppCompatActivity> AppCompatActivity.runThreadK(timer: Long = 1000) {
+    GlobalScope.launch {
+        delay(timer)
+        nextActivity<T>()
+        finish()
+    }
+}
 
-    val intent = Intent(context, clazz.java)
+inline fun <reified T : AppCompatActivity> Fragment.nextActivity(
+        extraKey: String = "", extra: Any = ""
+) {
+    val intent = Intent(activity, T::class.java)
 
     if (extraKey.isNotEmpty()) {
-        intent.putExtra(extraKey, extra)
+        when (extra) {
+            is String -> intent.putExtra(extraKey, extra)
+            is Boolean -> intent.putExtra(extraKey, extra)
+            is Float -> intent.putExtra(extraKey, extra)
+            is Long -> intent.putExtra(extraKey, extra)
+            is Int -> intent.putExtra(extraKey, extra)
+            is Bundle -> intent.putExtra(extraKey, extra)
+            else -> intent.putExtra(extraKey, "$extra")
+        }
     }
 
     startActivity(intent)
